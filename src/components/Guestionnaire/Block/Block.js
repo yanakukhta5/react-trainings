941c404cdb6225b2b19e2a76e9a3cd1d4cf8e96c
Yana Kukhta <kukhta0school@gmail.com>
2022-09-19 12:48:31 +0300

    bottom text

--- src/components/Guestionnaire/Block/Block.js
+++ src/components/Guestionnaire/Block/Block.js
@@ -1,54 +1,54 @@
-import React from "react";
-import { Guestion } from "../Guestion/Guestion";
-import { Summary } from "../Summary/Summary";
+import React from 'react'
+import { Guestion } from '../Guestion/Guestion'
+import { Summary } from '../Summary/Summary'
 
 class Block extends React.Component {
   constructor(props) {
-    super(props);
-    this.questionCount = this.props.questions.length;
+    super(props)
+    this.questionCount = this.props.questions.length
     this.state = {
       userAnswers: new Array(this.questionCount).fill({
-        answer: "",
-        isRight: false,
+        answer: '',
+        isRight: false
       }),
-      isRightClass: new Array(this.questionCount).fill("unknown"),
-      summaryShow: "hidden",
-      right: 0,
-    };
-    this.handlerInput = this.handlerInput.bind(this);
-    this.handlerSubmit = this.handlerSubmit.bind(this);
+      isRightClass: new Array(this.questionCount).fill('unknown'),
+      summaryShow: 'hidden',
+      right: 0
+    }
+    this.handlerInput = this.handlerInput.bind(this)
+    this.handlerSubmit = this.handlerSubmit.bind(this)
   }
-  
+
   handlerInput(text, n) {
     this.setState({
       userAnswers: this.state.userAnswers.map((elem, index) => {
-        if (n === index) return { ...elem, answer: text };
-        return { ...elem };
-      }),
-    });
+        if (n === index) return { ...elem, answer: text }
+        return { ...elem }
+      })
+    })
   }
 
   handlerSubmit(event) {
-    event.preventDefault();
-    const rightAnswers = this.props.questions.map((elem) => elem.answer);
-    const userAnswers = this.state.userAnswers;
-    let right = 0;
+    event.preventDefault()
+    const rightAnswers = this.props.questions.map((elem) => elem.answer)
+    const userAnswers = this.state.userAnswers
+    let right = 0
     userAnswers.forEach((elem, index) => {
       if (elem.answer === rightAnswers[index]) {
-        elem.isRight = true;
-        right++;
+        elem.isRight = true
+        right++
       } else {
-        elem.isRight = false;
+        elem.isRight = false
       }
-    });
+    })
     this.setState({
       isRightClass: this.state.isRightClass.map((elem, index) =>
-        JSON.parse(userAnswers[index]["isRight"]) ? "right" : "wrong"
-      ),
-    });
-    this.setState({ right: right });
-    this.setState({ summaryShow: "show" });
-    console.log(userAnswers);
+        JSON.parse(userAnswers[index]['isRight']) ? 'right' : 'wrong'
+      )
+    })
+    this.setState({ right: right })
+    this.setState({ summaryShow: 'show' })
+    console.log(userAnswers)
   }
 
   render() {
@@ -56,7 +56,7 @@ class Block extends React.Component {
       <div>
         <form>
           {this.props.questions.map((elem, index) => {
-            const answer = this.state.userAnswers[index];
+            const answer = this.state.userAnswers[index]
             return (
               <Guestion
                 key={index}
@@ -66,7 +66,7 @@ class Block extends React.Component {
                 value={answer.answer}
                 isRight={this.state.isRightClass[index]}
               />
-            );
+            )
           })}
           <input type="submit" onClick={this.handlerSubmit} />
         </form>
@@ -76,8 +76,8 @@ class Block extends React.Component {
           possible={this.props.questions.length}
         />
       </div>
-    );
+    )
   }
 }
 
-export { Block };
+export { Block }
